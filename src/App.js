9d33868b8f9ee6cbd1470041053acca51b336eb9
yunva17 <yunva17@naver.com>
2024-01-18 01:53:53 +0900

    fix: 회원가입 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/auth/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/auth/Login';
+import Signup from './pages/auth/Signup';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
